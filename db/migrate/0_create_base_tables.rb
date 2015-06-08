723bb0644d67cf70ef9b1513778e9a65a8922fb7
Alex Naser <c.a.naser@gmail.com>
2015-06-08 16:16:25 -0400

    adding migration to bootstrap tables

---
+++ db/migrate/0_create_base_tables.rb
@@ -0,0 +1,31 @@
+class CreateBaseTables < ActiveRecord::Migration
+
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.timestamps
+    end
+
+    create_table :posts do |t|
+      t.references :user
+      t.string :photo_url
+      t.timestamps
+    end
+
+    create_table :comments do |t|
+      t.references :user
+      t.text :text
+      t.timestamps
+    end
+
+    create_table :likes do |t|
+      t.references :user
+      t.references :post
+      t.timestamps
+    end
+
+  end
+
+end
