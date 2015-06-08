bbeea5b6723ca7433052d8ab54c6c193b5c97118
Alex Naser <c.a.naser@gmail.com>
2015-06-08 15:02:49 -0400

    methodizing humanized_time_ago

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,13 @@
+def humanized_time_ago(time_ago_in_minutes)
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutes ago"
+  end
+end
+
 # home
 get '/' do
+  time_ago_in_minutes = 500
+  erb humanized_time_ago(time_ago_in_minutes)
 end
