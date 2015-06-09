6a16db5a0b40c70ff34927ec1b86a70d1fd5c1d3
Alex Naser <c.a.naser@gmail.com>
2015-06-08 22:55:48 -0400

    adding :likes to associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
   has_many :comments
+  has_many :likes
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
+  has_many :likes
 
 end
