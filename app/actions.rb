6f6c79b729395c3112341cde1d43db9f20989102
Alex Naser <c.a.naser@gmail.com>
2015-06-08 22:56:03 -0400

    instance methods in Post

--- app/actions.rb
+++ app/actions.rb
@@ -1,11 +1,3 @@
-def humanized_time_ago(time_ago_in_minutes)
-  if time_ago_in_minutes >= 60
-    "#{time_ago_in_minutes / 60} hours ago"
-  else
-    "#{time_ago_in_minutes} minutes ago"
-  end
-end
-
 # home
 get '/' do
   @posts = [{ 
@@ -15,12 +7,12 @@ get '/' do
     }, 
     photo_url: "http://naserca.com/images/shark.jpg", 
     time_ago_in_minutes: humanized_time_ago(15), 
-    like_count: 12, 
+    like_count: 12,
     comments: [{
       user: {
         username: "sharky_j",
       },
-      text: "Out for the long weekend… too embarrassed to show y’all the beach bod!"
+      text: "Out for the long weekend... too embarrassed to show y’all the beach bod!"
     }, {
       user: {
         username: "mrs_sharksmith59",
@@ -34,7 +26,7 @@ get '/' do
     }, 
     photo_url: "http://naserca.com/images/whale.jpg", 
     time_ago_in_minutes: humanized_time_ago(72), 
-    like_count: 30, 
+    like_count: 30,
     comments: [{
       user: {
         username: "kirk_whalum",
@@ -48,7 +40,7 @@ get '/' do
     }, 
     photo_url: "http://naserca.com/images/marlin.jpg", 
     time_ago_in_minutes: humanized_time_ago(201), 
-    like_count: 28, 
+    like_count: 28,
     comments: [{
       user: {
         username: "marlin_peppa",
