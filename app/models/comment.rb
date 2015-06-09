05581f5f54594325db9def356499176b6e4a1a87
Alex Naser <c.a.naser@gmail.com>
2015-06-08 21:50:51 -0400

    adding associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :post
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
 class Like < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :post
+  
 end
