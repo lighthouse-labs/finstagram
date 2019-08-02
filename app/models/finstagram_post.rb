b547c89bde2b729074f2e4d2393e01e653b7f2a1
cameronkwan <cameronkwan@gmail.com>
2019-08-01 22:54:45 -0400

    wip

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -1,7 +1,10 @@
 class FinstagramPost < ActiveRecord::Base
   
+<<<<<<< Updated upstream
   belongs_to :user
   has_many :comment
   has_many :like
   
+=======
+>>>>>>> Stashed changes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   
+<<<<<<< Updated upstream
   has_many :finstagram_post
   has_many :like
   has_many :comment
   
+=======
+>>>>>>> Stashed changes
 end
