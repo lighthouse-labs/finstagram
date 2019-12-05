abe8e2df52412bfb71e8f7a497ce4570140afd8d
GaboCam <gabriel.camarena@yahoo.com>
2019-12-05 01:55:16 +0000

    W4D2 added form POST submit

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,43 @@
 get '/' do
 
- x=3
- y=5
- 
- (x+y).to_s
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
 
-(1..10).each do |i|
-    i.to_s
 end
 
+get '/signup' do   #if a user navigates to the path
+
+    @user = User.new  #setup empty @user object
+    erb(:signup)  #render "pp/views/signup.erb"
+
+end
+
+
+post '/signup' do
+    # "Form submitted!"
+
+#params.to_s
+
+#grab user input values
+email = params[:email]
+avatar_url = params[:avatar_url]
+username = params[:username]
+password = params[:password]
+
+#instntiate and save a User
+
+if email.present? && avatar_url.present? && username.present? && password.present?
+
+#instantiate and save a user    
+    user = User.new({email: email, avatar_url:avatar_url, username: username, password: password})
+    user.save
+
+#return readable representation of user objectc
+escape_html user.inspect
+
+else
+
+#display simple error
+"Validation failed"    
+end
 end
---
+++ app/models/like.rb
@@ -0,0 +1,5 @@
+class Like <ActiveRecord::Base
+
+    belongs_to :user
+    belongs_to :finstagram_post
+end
