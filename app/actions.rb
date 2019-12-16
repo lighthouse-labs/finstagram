bc87c135343ac85465da87a4c9e21cea796f546a
tanyacode <tzelenyuk@protonmail.com>
2019-12-16 22:01:23 +0000

    added login and signup

--- app/actions.rb
+++ app/actions.rb
@@ -7,13 +7,40 @@ post '/login' do
   username = params[:username]
   password = params[:password]
 
-  user = User.find_by(username: username)  
+  @user = User.find_by(username: username)  
 
-  if user && user.password == password
-    "Success!"
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    "Success! User with id #{session[:user_id]} is logged in!"
   else
-    "Login failed."
+    @error_message = "Login failed."
+    erb(:login)
   end
+end
+
+get '/login' do    # when a GET request comes into /login
+  erb(:login)      # render app/views/login.erb
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  "Logout successful!"
+end
+
+post '/signup' do
+
+  # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  # instantiate and save a User
+  user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  user.save
+
+  # return readable representation of User object
+  escape_html user.inspect
 end
 
 get '/signup' do     # if a user navigates to the path "/signup",
