a119b96cbcfee2efeb34d97231ac6724c89fab5a
tanyacode <tzelenyuk@protonmail.com>
2019-12-16 21:02:06 +0000

    add db, actions, models

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,7 @@
 get '/' do
   
 
-    @finstagram_post_shark = {
+@finstagram_post_shark = {
   username: "sharky_j",
   avatar_url: "http://naserca.com/images/sharky_j.jpg",
   photo_url: "http://naserca.com/images/shark.jpg",
@@ -46,4 +46,27 @@ get '/' do
  
   
     erb(:index)
+end
+
+get '/' do
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    "Success!"
+  else
+    "Login failed."
+  end
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
 end
