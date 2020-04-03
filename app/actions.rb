0c70cb91b359c90b1d1e74650bcfc7b379b426fb
vkfmvp777 <vkfmvp777@gmail.com>
2020-04-03 04:08:44 +0000

    Readies app for deployment

--- app/actions.rb
+++ app/actions.rb
@@ -74,3 +74,18 @@ get '/finstagram_posts/:id' do
   @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
   erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
 end
+
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
