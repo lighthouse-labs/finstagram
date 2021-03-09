5d1b29c2dda60eabede710d4a4accdcfac34467a
forer44 <evan@teifi.ca>
2021-03-09 04:28:23 +0000

    create array finstagram posts & looped thru posts

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,6 @@
 def humanized_time_ago(time_ago_in_minutes)
   if time_ago_in_minutes >= 60
-    "#{time_ago_in_minutes} / 60} hours ago"
+    "#{time_ago_in_minutes/ 60} hours ago"
   else
     "#{time_ago_in_minutes} minutes ago"
   end
@@ -47,6 +47,8 @@ get '/' do
      }
 
 
-  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+  
   erb(:index)
+
 end
