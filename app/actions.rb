5bbb22b7355d54f204eb02ac9f2df3838948311c
forer44 <evan@teifi.ca>
2021-03-18 01:55:48 +0000

    added form

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,24 @@ get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
 end
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end
+
+post '/signup' do
+    
+    email       =params[:email]
+    avatar_url  =params[:avatar_url]
+    username    =params[:username]
+    password    =params[:password]
+    
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    
+    if @user.save
+        "User #{username} saved!"
+    else
+        erb(:signup)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
     has_many :comments
     has_many :likes
 
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
+
 end
