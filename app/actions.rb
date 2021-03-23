5a28ac18fbc6ebbf8e51abae4a18f8630596e59d
forer44 <evan@teifi.ca>
2021-03-23 02:21:37 +0000

    added login / signup and logic for redirection upon login /logout

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do 
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
@@ -8,17 +14,41 @@ get '/signup' do
     erb(:signup)
 end
 
+get '/login' do
+    erb(:login)
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+post '/login' do
+    username    = params[:username]
+    password    = params[:password]
+
+    @user = User.find_by(username: username)
+
+    if @user && @user.password == password
+        session[:user_id] = @user.id
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
 post '/signup' do
     
-    email       =params[:email]
-    avatar_url  =params[:avatar_url]
-    username    =params[:username]
-    password    =params[:password]
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
     
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
     
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
     end
