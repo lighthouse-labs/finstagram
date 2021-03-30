d7c6acad60af77bb7b680f11f77b44de3121c4f8
forer44 <evan@teifi.ca>
2021-03-30 02:11:38 +0000

    made comments and likes work and stuff

--- app/actions.rb
+++ app/actions.rb
@@ -76,3 +76,23 @@ post '/finstagram_posts' do
     end
 end
 
+post '/comments' do
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  comment.save
+  redirect(back)
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ActiveRecord::Base
     belongs_to :user
     belongs_to :finstagram_post
 
+    validates_presence_of :user, :finstagram_post
+
 end
